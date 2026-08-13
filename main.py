28552a4e83fa9912853a93ae9aa348817472ceb7
Eugênio Quintas <eugenioquintas20@gmail.com>
2026-08-13 20:03:02 +0100

    fix: save_as_csv and adding main

---
+++ main.py
@@ -0,0 +1,6 @@
+from exporter import save_as_csv
+from fetcher import fetch_html
+from parser import parsing_html
+
+def run():
+    print()
--- parser.py
+++ parser.py
@@ -1,12 +1,12 @@
 from bs4 import BeautifulSoup
 
-def parsing_html(html_content: str) -> list(dict[str, str]):
+def parsing_html(html_content: str) -> list[dict[str, str]]:
     soup = BeautifulSoup(html_content, 'html.parser')
-    noticias = []
+    noticias: list[dict[str, str]] = []
     artigos = soup.find_all("a", class_= "oop-card__title-link")
     for artigo in artigos:
         titulo = artigo.get_text(strip=True)
-        link = artigo.get("href", "")
+        link = str(artigo.get("href") or "")
 
         if titulo and link:
             noticias.append({
